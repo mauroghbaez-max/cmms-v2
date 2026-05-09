9b5944387071bbb037d0f1367461dbbe331f3997
Mauro Herrera <gbaez@ev-servicios.com>
2026-05-09 07:53:38 -0300

    fix: add StaticFiles import

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from app.db.session import engine, Base
 from app.db.models import *
